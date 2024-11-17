72edb75385cdafb0e0f3adafff692c295d9c79ed
Shreyash Parajuli <shreyashbparajuli@gmail.com>
2024-11-17 23:17:15 +0545

    Added Input and submit button

--- ShapeForm/ShapeForm/Form1.Designer.cs
+++ ShapeForm/ShapeForm/Form1.Designer.cs
@@ -28,21 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.Submit = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "No. of Shapes: ";
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(118, 25);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(113, 20);
+            this.textBox.TabIndex = 1;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // Submit
+            // 
+            this.Submit.Location = new System.Drawing.Point(237, 25);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(72, 20);
+            this.Submit.TabIndex = 2;
+            this.Submit.Text = "Submit";
+            this.Submit.UseVisualStyleBackColor = true;
+            this.Submit.Click += new System.EventHandler(this.Submit_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1469, 629);
+            this.ClientSize = new System.Drawing.Size(509, 432);
+            this.Controls.Add(this.Submit);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button Submit;
     }
 }
 
--- ShapeForm/ShapeForm/Form1.cs
+++ ShapeForm/ShapeForm/Form1.cs
@@ -4,11 +4,92 @@ using System.Windows.Forms;
 
 namespace ShapeForm
 {
+    public partial class Form1 : Form
+    {
+        private Shape[] shapes = new Shape[100]; // Initial array size
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Randomly generate initial shapes (if needed)
+            Random random = new Random();
+            for (int i = 0; i < shapes.Length; i++)
+            {
+                if (random.Next(2) == 0)
+                {
+                    shapes[i] = new Circle(random.Next(20, 320), random.Next(20, 320), random.Next(10, 50));
+                }
+                else
+                {
+                    shapes[i] = new Rectangle(random.Next(20, 320), random.Next(20, 320), random.Next(20, 100), random.Next(20, 100));
+                }
+            }
+
+            // Attach paint event
+            this.Paint += Form1_Paint;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Optional load logic
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            foreach (var shape in shapes)
+            {
+                shape?.Draw(g);
+            }
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            // Parse the number of shapes from the textbox
+            int numberOfShapes;
+            if (int.TryParse(textBox.Text, out numberOfShapes) && numberOfShapes > 0 && numberOfShapes <= 100)
+            {
+                Random random = new Random();
+                shapes = new Shape[numberOfShapes]; // Adjust the array size
+
+                // Populate the shapes array
+                for (int i = 0; i < numberOfShapes; i++)
+                {
+                    if (random.Next(2) == 0)
+                    {
+                        shapes[i] = new Circle(random.Next(20, 300), random.Next(20, 300), random.Next(20, 50));
+                    }
+                    else
+                    {
+                        shapes[i] = new Rectangle(random.Next(20, 300), random.Next(20, 300), random.Next(40, 100), random.Next(40, 100));
+                    }
+                }
+
+                // Trigger a repaint to draw the new shapes
+                this.Invalidate();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a number between 1 and 100.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            // Example logic for handling text change (you can customize this)
+            int numberOfShapes;
+            Submit.Enabled = int.TryParse(textBox.Text, out numberOfShapes) && numberOfShapes > 0 && numberOfShapes <= 100;
+        }
+    }
+
+    // Define the Shape interface
     public interface Shape
     {
         void Draw(Graphics g);
     }
 
+    // Circle class implementing the Shape interface
     public class Circle : Shape
     {
         private int X, Y, Radius;
@@ -26,6 +107,7 @@ namespace ShapeForm
         }
     }
 
+    // Rectangle class implementing the Shape interface
     public class Rectangle : Shape
     {
         private int X, Y, Width, Height;
@@ -41,45 +123,6 @@ namespace ShapeForm
         public void Draw(Graphics g)
         {
             g.DrawRectangle(Pens.Black, X, Y, Width, Height);
-        }
-    }
-
-    public partial class Form1 : Form
-    {
-        private Shape[] shapes = new Shape[100];
-
-        public Form1()
-        {
-            InitializeComponent();
-            Random random = new Random();
-
-            for (int i = 0; i < shapes.Length; i++)
-            {
-                if (random.Next(2) == 0)
-                {
-                    shapes[i] = new Circle(random.Next(10, 300), random.Next(10, 300), random.Next(10, 50));
-                }
-                else
-                {
-                    shapes[i] = new Rectangle(random.Next(10, 300), random.Next(10, 300), random.Next(20, 100), random.Next(20, 100));
-                }
-            }
-
-            this.Paint += Form1_Paint;
-        }
-
-        private void Form1_Paint(object sender, PaintEventArgs e)
-        {
-            Graphics g = e.Graphics;
-            foreach (var shape in shapes)
-            {
-                shape?.Draw(g);
-            }
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            
         }
     }
 }
