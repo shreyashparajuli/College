8dab42611c8e10d6f655efddbb448775c82fc49d
Shreyash Parajuli <shreyashbparajuli@gmail.com>
2024-11-17 23:23:45 +0545

    Added inputTxtBox and submitButton

--- ShapeForm/ShapeForm/Form1.cs
+++ ShapeForm/ShapeForm/Form1.cs
@@ -6,13 +6,13 @@ namespace ShapeForm
 {
     public partial class Form1 : Form
     {
-        private Shape[] shapes = new Shape[100]; // Initial array size
+        private Shape[] shapes = new Shape[100]; 
 
         public Form1()
         {
             InitializeComponent();
 
-            // Randomly generate initial shapes (if needed)
+            
             Random random = new Random();
             for (int i = 0; i < shapes.Length; i++)
             {
@@ -26,13 +26,13 @@ namespace ShapeForm
                 }
             }
 
-            // Attach paint event
+      
             this.Paint += Form1_Paint;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            // Optional load logic
+     
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
@@ -46,14 +46,14 @@ namespace ShapeForm
 
         private void Submit_Click(object sender, EventArgs e)
         {
-            // Parse the number of shapes from the textbox
+ 
             int numberOfShapes;
             if (int.TryParse(textBox.Text, out numberOfShapes) && numberOfShapes > 0 && numberOfShapes <= 100)
             {
                 Random random = new Random();
-                shapes = new Shape[numberOfShapes]; // Adjust the array size
+                shapes = new Shape[numberOfShapes]; 
 
-                // Populate the shapes array
+   
                 for (int i = 0; i < numberOfShapes; i++)
                 {
                     if (random.Next(2) == 0)
@@ -66,7 +66,6 @@ namespace ShapeForm
                     }
                 }
 
-                // Trigger a repaint to draw the new shapes
                 this.Invalidate();
             }
             else
@@ -77,19 +76,18 @@ namespace ShapeForm
 
         private void textBox_TextChanged(object sender, EventArgs e)
         {
-            // Example logic for handling text change (you can customize this)
+
             int numberOfShapes;
             Submit.Enabled = int.TryParse(textBox.Text, out numberOfShapes) && numberOfShapes > 0 && numberOfShapes <= 100;
         }
     }
 
-    // Define the Shape interface
     public interface Shape
     {
         void Draw(Graphics g);
     }
 
-    // Circle class implementing the Shape interface
+
     public class Circle : Shape
     {
         private int X, Y, Radius;
@@ -107,7 +105,6 @@ namespace ShapeForm
         }
     }
 
-    // Rectangle class implementing the Shape interface
     public class Rectangle : Shape
     {
         private int X, Y, Width, Height;
