01e7096d7d59b8293d53f25b8013d83cea4e0cd6
Shreyash Parajuli <shreyashbparajuli@gmail.com>
2024-11-17 21:06:45 +0545

    Abstract

---
+++ ShapeForm/ShapeForm/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ShapeForm
+{
+    public interface Shape
+    {
+        void Draw(Graphics g);
+    }
+
+    public class Circle : Shape
+    {
+        private int X, Y, Radius;
+
+        public Circle(int x, int y, int radius)
+        {
+            X = x;
+            Y = y;
+            Radius = radius;
+        }
+
+        public void Draw(Graphics g)
+        {
+            g.DrawEllipse(Pens.Black, X - Radius, Y - Radius, Radius * 2, Radius * 2);
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        private int X, Y, Width, Height;
+
+        public Rectangle(int x, int y, int width, int height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
+
+        public void Draw(Graphics g)
+        {
+            g.DrawRectangle(Pens.Black, X, Y, Width, Height);
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        private Shape[] shapes = new Shape[100];
+
+        public Form1()
+        {
+            InitializeComponent();
+            Random random = new Random();
+
+            for (int i = 0; i < shapes.Length; i++)
+            {
+                if (random.Next(2) == 0)
+                {
+                    shapes[i] = new Circle(random.Next(10, 300), random.Next(10, 300), random.Next(10, 50));
+                }
+                else
+                {
+                    shapes[i] = new Rectangle(random.Next(10, 300), random.Next(10, 300), random.Next(20, 100), random.Next(20, 100));
+                }
+            }
+
+            this.Paint += Form1_Paint;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            foreach (var shape in shapes)
+            {
+                shape?.Draw(g);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ ShapeForm/ShapeForm/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ShapeForm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ShapeForm")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("119b91b2-739e-4105-a3ee-9546d0c8e387")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
